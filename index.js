5200ce6f55fc4d3eb1b7892b8687b4ef1af807b6
Shir-lap <mejiaacostashirlipao@gmail.com>
2024-03-11 06:34:58 -0500

    actializancion

--- index.js
+++ index.js
@@ -1,79 +1,151 @@
-// Importación de los módulos necesarios
-const express = require('express'); // Módulo para crear el servidor Express
-const mongoose = require('mongoose'); // Módulo para interactuar con MongoDB
+// // Importación de los módulos necesarios
+// const express = require('express'); // Módulo para crear el servidor Express
+// const mongoose = require('mongoose'); // Módulo para interactuar con MongoDB
 
-// Conexión a la base de datos MongoDB Atlas
+// // Conexión a la base de datos MongoDB Atlas
+// mongoose.connect('mongodb+srv://pipe2893:GVjln1bdayemMJxr@empleo.msfwmfh.mongodb.net/');
+
+// // Obtiene la conexión a la base de datos y maneja los errores
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'Error de conexión en la base de datos'));
+
+// // Una vez que la conexión a la base de datos se realiza con éxito, se ejecuta este callback
+// db.once('open', function(){
+//     console.log('Conectado a MongoDB');
+
+//     // Define el esquema del usuario utilizando Mongoose
+//     const userSchema = mongoose.Schema({
+//         nombres: String,
+//         apellidos: String
+//     });
+
+//     // Crea el modelo 'User' a partir del esquema definido
+//     const User = mongoose.model('User', userSchema);
+
+//     // Crea una instancia de la aplicación Express
+//     const app = express();
+
+//     // Middleware para parsear las solicitudes JSON
+//     app.use(express.json());
+
+//     ///1. listado de todos los usuarios- /api/users
+//     app.get('/api/users', async (req, res) => {
+//         // Busca todos los usuarios en la base de datos
+//         const users = await User.find();
+//         // Envia los usuarios como respuesta en formato JSON
+//         res.json(users)
+//         // console.log(users);
+//     });
+
+//     //2. listado de solo 10 usuarios- /api/users/limit
+//     app.get('/api/users?limit=10', async (req, res) => {
+//         // Busca todos los usuarios en la base de datos
+//         const users = await User.find();
+//         // Envia los usuarios como respuesta en formato JSON
+//         res.json(users)
+//     });
+
+//     //3. listado de todas las empresas- /api/companies
+//     app.get('/api/empresas', async (req, res) => {
+//         // Busca todos los usuarios en la base de datos
+//         const users = await User.find();
+//         // Envia los usuarios como respuesta en formato JSON
+//         res.json(users)
+//         // console.log(users);
+//     });
+
+//     //4. listado de usuarios que sea de la empresa id 5- api/users/companies/5
+//     app.get('/api/users/empresas/5', async (req, res) => {
+//         // Busca todos los usuarios en la base de datos
+//         const users = await User.find();
+//         // Envia los usuarios como respuesta en formato JSON
+//         res.json(users)
+//         // console.log(users);
+//     });
+
+//     //5. listado de usuarios que sean de bangladesh- /api/companies/city/Bangladesh
+//     app.get('/api/empresas/ciudad/Bangladesh', async (req, res) => {
+//         // Busca todos los usuarios en la base de datos
+//         const users = await User.find();
+//         // Envia los usuarios como respuesta en formato JSON
+//         res.json(users)
+//         // console.log(users);
+//     });
+
+//     // Inicia el servidor Express en el puerto 3000
+//     app.listen(3000, function(){
+//         console.log('Servidor arriba');
+//     });
+// });
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+
 mongoose.connect('mongodb+srv://pipe2893:GVjln1bdayemMJxr@empleo.msfwmfh.mongodb.net/');
 
-// Obtiene la conexión a la base de datos y maneja los errores
 const db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'Error de conexión en la base de datos'));
 
-// Una vez que la conexión a la base de datos se realiza con éxito, se ejecuta este callback
-db.once('open', function(){
-    console.log('Conectado a MongoDB');
+db.once('open', () => {
+    console.log("DB conected");
 
-    // Define el esquema del usuario utilizando Mongoose
-    const userSchema = mongoose.Schema({
+    const userShema = mongoose.Schema({
         nombres: String,
         apellidos: String
     });
+    
+    const User = mongoose.model('User', userShema);
+    const Empresa = mongoose.model('Empresas', userShema);
 
-    // Crea el modelo 'User' a partir del esquema definido
-    const User = mongoose.model('User', userSchema);
-
-    // Crea una instancia de la aplicación Express
     const app = express();
 
-    // Middleware para parsear las solicitudes JSON
     app.use(express.json());
 
-    ///1. listado de todos los usuarios- /api/users
     app.get('/api/users', async (req, res) => {
-        // Busca todos los usuarios en la base de datos
         const users = await User.find();
-        // Envia los usuarios como respuesta en formato JSON
-        res.json(users)
-        // console.log(users);
-    });
-
-    //2. listado de solo 10 usuarios- /api/users/limit
-    app.get('/api/users?limit=10', async (req, res) => {
-        // Busca todos los usuarios en la base de datos
-        const users = await User.find();
-        // Envia los usuarios como respuesta en formato JSON
         res.json(users)
     });
 
-    //3. listado de todas las empresas- /api/companies
+    app.get('/api/users/limit', async (req, res) => {
+        const users = await User.find().limit(10);
+        res.json(users)
+    });
+    
+    app.get('/api/users/country/:region', async (req, res) => {
+        const users = await User.find({ciudad: req.params.region});
+        res.json(users)
+    });
+
     app.get('/api/empresas', async (req, res) => {
-        // Busca todos los usuarios en la base de datos
-        const users = await User.find();
-        // Envia los usuarios como respuesta en formato JSON
-        res.json(users)
-        // console.log(users);
+        const empresas = await Empresa.find();
+        res.json(empresas)
     });
 
-    //4. listado de usuarios que sea de la empresa id 5- api/users/companies/5
-    app.get('/api/users/empresas/5', async (req, res) => {
-        // Busca todos los usuarios en la base de datos
-        const users = await User.find();
-        // Envia los usuarios como respuesta en formato JSON
-        res.json(users)
-        // console.log(users);
+    app.get('/api/empresas/:id', async (req, res) => {
+        const id = parseInt(req.params.id)
+        const empresas = await Empresa.find({id: id});
+        res.json(empresas)
     });
 
-    //5. listado de usuarios que sean de bangladesh- /api/companies/city/Bangladesh
-    app.get('/api/empresas/ciudad/Bangladesh', async (req, res) => {
-        // Busca todos los usuarios en la base de datos
-        const users = await User.find();
-        // Envia los usuarios como respuesta en formato JSON
-        res.json(users)
-        // console.log(users);
+    app.get('/api/empresas/city/:ciudad', async (req, res) => {
+        const empresas = await Empresa.find({ciudad: req.params.ciudad});
+        res.json(empresas)
     });
 
-    // Inicia el servidor Express en el puerto 3000
-    app.listen(3000, function(){
-        console.log('Servidor arriba');
+    app.get('/', (req, res) => {
+        res.json({
+            message: 'Hello World!',
+            users: '/api/users',
+            empresas: '/api/empresas'
+        })
     });
+
+    app.listen(3001, () => {
+        console.log("Servidor corriendo en el puerto 3001");
+    });
+
 });
+
+console.log('HOLA');
